77ce1d123720b83691158f87acc91128edb6dd75
Sean Kramer <seandominickramer@gmail.com>
2020-06-03 20:00:32 -0400

    Add constructor to hold state, and addItem function to set state in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+addItem = (e, item) => {
+  e.preventDefault();
+  const newItem = {
+    task: item,
+    id: Date.now(),
+    completed: false
+  };
+  this.setState({
+    todos: [...this.state.todos, newItem]
+  })
+}
+
   render() {
     return (
       <div>
