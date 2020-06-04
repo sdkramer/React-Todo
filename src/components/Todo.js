f650ff483f03dfe32cdcbbce39785a5c02870009
Sean Kramer <seandominickramer@gmail.com>
2020-06-04 14:06:01 -0400

    Add functionality to toggleItem

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,7 @@ import './Todo.css'
 const Todo = props => {
   console.log(props)
   return(
-<div className={`item${props.item.completed ? 'completed' : ''}`} onClick={() => props.toggleItem(props.item.id)}>
+<div className={`${props.item.completed ? 'completed' : ''}`} onClick={() => props.toggleItem(props.item.id)}>
 <p>{props.item.task}</p>
 </div>
   )
